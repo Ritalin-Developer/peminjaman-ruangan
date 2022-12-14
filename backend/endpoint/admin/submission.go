687d830f71badfebe8b124977c0fa597744d237f
Arie Brainware <publicu001@protonmail.com>
2022-12-15 01:35:58 +0700

    Implement submission list endpoint for admin

--- backend/endpoint/admin/submission.go
+++ backend/endpoint/admin/submission.go
@@ -1,13 +1,35 @@
 package endpoint
 
 import (
+	"github.com/ITEBARPLKelompok3/peminjaman-ruangan/backend/external"
+	"github.com/ITEBARPLKelompok3/peminjaman-ruangan/backend/model"
 	"github.com/ITEBARPLKelompok3/peminjaman-ruangan/backend/util"
+	"github.com/getsentry/sentry-go"
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
 
 func SubmissionList(c *gin.Context) {
-	util.CallSuccessOK(c, "success", nil)
+	db, err := external.GetPostgresClient()
+	if err != nil {
+		log.Error(err)
+		sentry.CaptureException(err)
+		util.CallServerError(c, "something wrong, please try again later", err)
+		return
+	}
+	submission := []*model.Submission{}
+	err = db.
+		Model(&submission).
+		Find(&submission).
+		Error
+	if err != nil {
+		log.Error(err)
+		util.CallServerError(c, "something wrong, please try again later", err)
+		return
+	}
+	util.CallSuccessOK(c, "success", submission)
 }
+
 func SubmissionApprove(c *gin.Context) {
 	util.CallSuccessOK(c, "success", nil)
 }
