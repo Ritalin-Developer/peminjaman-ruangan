0fb3a3c35bceae12ae9f6b8b3d2a905ee0f693ad
Arie Brainware <publicu001@protonmail.com>
2022-12-15 17:27:12 +0700

    Add user room list endpoint

--- backend/endpoint/user/room.go
+++ backend/endpoint/user/room.go
@@ -1,10 +1,45 @@
 package endpoint
 
 import (
+	"strconv"
+
+	"github.com/ITEBARPLKelompok3/peminjaman-ruangan/backend/external"
+	"github.com/ITEBARPLKelompok3/peminjaman-ruangan/backend/model"
 	"github.com/ITEBARPLKelompok3/peminjaman-ruangan/backend/util"
+	"github.com/getsentry/sentry-go"
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
 
 func UserRoomList(c *gin.Context) {
-	util.CallSuccessOK(c, "success", nil)
+	// TODO: Implement search by query/filter
+	_limit := c.Query("limit")
+	limit, err := strconv.Atoi(_limit)
+	if err != nil {
+		log.Error(err)
+		sentry.CaptureException(err)
+		util.CallUserError(c, "must provide limit query", err)
+		return
+	}
+
+	db, err := external.GetPostgresClient()
+	if err != nil {
+		log.Error(err)
+		sentry.CaptureException(err)
+		util.CallServerError(c, "something wrong, please try again later", err)
+		return
+	}
+	rooms := []*model.Room{}
+	err = db.
+		Model(&rooms).
+		Limit(limit).
+		Find(&rooms).
+		Error
+	if err != nil {
+		log.Error(err)
+		util.CallServerError(c, "something wrong, please try again later", err)
+		return
+	}
+
+	util.CallSuccessOK(c, "success", rooms)
 }
